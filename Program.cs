da2df8b09d511c3f7a554662bc3052d6d938b301
unknown <lrenau911e@TP117.ensc.fr>
2016-01-05 15:01:06 +0100

    code
    code ma gueule

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace projet_informatique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[][] Clavier = new string[13][];
+            for (int i = 0; i < Clavier.Length; i++)
+            {
+                Clavier[i] = new string[4];
+
+            }
+
+
+
+            int i = 0;
+            while (i<=4)
+            {
+                Console.Write(Caractères[i]);
+                if (i == 4)
+                    i = 0;
+            }
+
+        }
+    }
+}
