c3f2a61942213112109ba867a8a9781eb01c29d7
ManonBonnet-Save <mbonnets@ensc.fr>
2016-01-05 15:04:31 +0100

    Touche de féminité au code ma gueule

--- Program.cs
+++ Program.cs
@@ -7,6 +7,19 @@ namespace projet_informatique
 {
     class Program
     {
+        public static void multi_tap (string message)
+		{
+		}
+
+		public static void t9 (string message)
+		{
+		}
+
+		public static void messageErreur()
+		{
+			Console.WriteLine ("Votre saisie est incorrecte");
+		}
+		
         static void Main(string[] args)
         {
 
@@ -26,7 +39,31 @@ namespace projet_informatique
                 if (i == 4)
                     i = 0;
             }
+            
+            
+            string sms;
+			Console.WriteLine ("Veuillez entrer un mode de saisie intuitive: (multi_tap ou t9)");
+			string message = Console.ReadLine ();
 
-        }
+			bool saisieCorrecte = false;
+			while (!saisieCorrecte) {
+				messageErreur ();
+				Console.WriteLine ("Veuillez entrer un mode de saisie intuitive: (multi_tap ou t9)");
+				message = Console.ReadLine ();
+				if (message != "multi_tap" && message != "t9")
+					messageErreur ();
+				
+				else
+					saisieCorrecte = true;
+			}
+			
+			Console.WriteLine ("Veuillez entrer un message à convertir:");
+			sms = Console.ReadLine ();
+
+			if (message == "multi_tap")
+			multi_tap (sms);
+			if (message == "t9")
+				t9 (sms);
+        }// Fin du Main
     }
 }
