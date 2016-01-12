67817162b7a87329cf06a1c405d2098d0e6576c4
ManonBonnet-Save <mbonnets@ensc.fr>
2016-01-12 15:33:08 +0100

    cide

--- projet informatique/projet informatique/Program.cs
+++ projet informatique/projet informatique/Program.cs
@@ -5,8 +5,8 @@ using System.Text;
 
 namespace projet_informatique
 {
-    class Program
-    {
+	class Program
+	{
 		public static void multi_tap (string message)
 		{
 		}
@@ -36,10 +36,10 @@ namespace projet_informatique
 			string[] diese = new string[]{"1"};
 			string[] etoile = new string[]{"1"};*/
 
-			static void Main(string[] args)
-			{
+		static void Main(string[] args)
+		{
 
-				/*string[][] Clavier = new string[14][]; // Création du tableau de tableau
+			/*string[][] Clavier = new string[14][]; // Création du tableau de tableau
 			/*Clavier[1] = new string[]{".",",", "-", "?", "!", "", ":", "(", ")", "/", "1"};
 			Clavier[2] = new string[]{"a", "b", "c", "à", "ä", "ç", "2"};
 			Clavier[3] = new string[]{"d", "e", "f", "è", "é", "3"};
@@ -54,7 +54,7 @@ namespace projet_informatique
 			Clavier[12] = new string[]{"1"};
 			Clavier[13] = new string[]{"1"};*/
 
-                string[] touche1 = new string[] {".",",","-", "?", "!", "@", ":", "(", ")", "/", "1"};
+			string[] touche1 = new string[] {".",",","-", "?", "!", "@", ":", "(", ")", "/", "1"};
 			string[] touche2 = new string[] {"a", "b", "c", "à", "ä", "ç", "2"};
 			string [] touche3 = new string[]{"d", "e", "f", "è", "é", "3"};
 			string[] touche4 = new string[]{"g", "h", "i", "4"};
@@ -67,13 +67,13 @@ namespace projet_informatique
 			string[] space = new string[]{" "};
 			string[] diese = new string[]{"1"};
 			string[] etoile = new string[]{"1"};
-                string[][] Clavier =
-                {touche1,touche2,touche3,touche4,touche5,touche6,touche7,touche8,touche9,touche0,space, diese, etoile};
+			string[][] Clavier =
+			{touche1,touche2,touche3,touche4,touche5,touche6,touche7,touche8,touche9,touche0,space, diese, etoile};
 
 
 
 
-				/*int i = 0;
+			/*int i = 0;
 				while (i<=4)
 				{
 					Console.Write(Caractères[i]);
@@ -82,72 +82,110 @@ namespace projet_informatique
 				}*/
 
 
-				string sms;
-				//Console.WriteLine("Veuillez entrer un mode de saisie intuitive: (multi_tap ou t9)");
-				string message="";
+			string sms;
+			//Console.WriteLine("Veuillez entrer un mode de saisie intuitive: (multi_tap ou t9)");
+			string message="";
 
-				bool saisieCorrecte = false;
-				while (!saisieCorrecte) {
-					Console.WriteLine ("Veuillez entrer un mode de saisie intuitive: (multi_tap ou t9)");
-					message = Console.ReadLine ();
-					if (message != "multi_tap" && message != "t9")
-						messageErreur ();
+			bool saisieCorrecte = false;
+			while (!saisieCorrecte)
+			{
+				Console.WriteLine ("Veuillez entrer un mode de saisie intuitive: (multi_tap ou t9)");
+				message = Console.ReadLine ();
+				if (message != "multi_tap" && message != "t9")
+					messageErreur ();
 
-					else
-						saisieCorrecte = true;
-				}
+				else
+					saisieCorrecte = true;
+			}
 
-				Console.WriteLine ("Veuillez entrer un message à convertir:");
-				sms = Console.ReadLine ();
-				int LongueurSms = sms.Length;
+			Console.WriteLine ("Veuillez entrer un message à convertir:");
+			sms = Console.ReadLine ();
+			int LongueurSms = sms.Length;
 
-				string[] SmsTab = new string[LongueurSms];
-				for (int i = 0; i < SmsTab.Length; i++) 
-                {
-					string Car = sms.Substring (i, 1);
-					SmsTab [i] = Car;
+			string[] SmsTab = new string[LongueurSms];
 
-					for (int n = 0; n<SmsTab.Length;n++)
-					{
-						for (int p = 0; p<Clavier.Length; p++)
-                        {
-                        if (Car[n] = Clavier[p][j])
-                        {}//&& Car[i] = "2" && Car [i] = "3" || Car [i] = "4" || Car [i] = "5" || Car [i] = "6" || Car [i] = "7" || Car [i] = "8" || Car [i] = "9" || Car [i] = "0" || Car [i] = "*" || Car [i] = "#"*/)
-							//messageErreur ();
-                        }
+			int j =0;
+
+			string[] retranscrit = new string[LongueurSms];
+
+			for (int i = 0; i < SmsTab.Length; i++) 
+			{
+				string Car = sms.Substring (i, 1);
+				SmsTab [i] = Car;
+			}
+
+			Console.WriteLine ("%%%%");
+
+			for (int t = 0; t < SmsTab.Length; t++) {
+				for (int n = 0; n < SmsTab.Length - 1; n++) {
+					if (SmsTab [n] == SmsTab [n + 1]) {
+						j++;
 					}
-
-                    // Recherche bon caractère
-                int j =0;
-                int z=0;
-                while (Car[z]==Car[z+1])
-                {
-                    j++;
-
-                }
-                
-                string lettre= Clavier{touche3[j]};
 				}
-				for (int i =0; i<SmsTab.Length; i++)
-					Console.WriteLine(SmsTab[i]);
 
-                
+				for (int n = 0; n < SmsTab.Length; n++) {
+					if (SmsTab [n] == "0") {
+						retranscrit [n] = touche0 [j];
+					}
+					if (SmsTab [n] == "1") {
+						retranscrit [n] = touche1 [j];
+					}
+					if (SmsTab [n] == "2") {
+						retranscrit [n] = touche2 [j];
+					}
+					if (SmsTab [n] == "3") {
+						retranscrit [n] = touche3 [j];
+					}
+					if (SmsTab [n] == "4") {
+						retranscrit [n] = touche4 [j];
+					}
+					if (SmsTab [n] == "5") {
+						retranscrit [n] = touche5 [j];
+					}
+					if (SmsTab [n] == "6") {
+						retranscrit [n] = touche6 [j];
+					}
+					if (SmsTab [n] == "7") {
+						retranscrit [n] = touche7 [j];
+					}
+					if (SmsTab [n] == "8") {
+						retranscrit [n] = touche8 [j];
+					}
+					if (SmsTab [n] == "9") {
+						retranscrit [n] = touche9 [j];
+					}
+					if (SmsTab [n] == "*") {
+						retranscrit [n] = etoile [j];
+					}
+					if (SmsTab [n] == "#") {
+						retranscrit [n] = diese [j];
+					}
+					if (SmsTab [n] == " ") {
+						retranscrit [n] = space [j];
+					}
+				}
+				j = 0;
+				Console.Write (retranscrit [t]);
+			}
+			// Recherche bon caractère
 
-				//Comparaison entre le tableau d'entrée et le double tableau
-				int j = 0;
-				int l = 0;
-				while (l < Clavier.Length)
+
+			//string lettre= Clavier.touche3[j];
+
+			//Comparaison entre le tableau d'entrée et le double tableau
+			int r = 0;
+			int l = 0;
+			while (l < Clavier.Length)
+			{
+				if (SmsTab[r] == touche1[l])
 				{
-					if (SmsTab[j] = Clavier[l])
-					{
 
-					}
 				}
-				/*if (message == "multi_tap")
+			}
+			/*if (message == "multi_tap")
 				multi_tap (sms);
 			if (message == "t9")
 				t9 (sms);*/
-			}// Fin du Main
-		}
+		}// Fin du Main
 	}
 }
