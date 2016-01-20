984aaffa0c3e862f6db9f746aaea70f5af63042f
ManonBonnet-Save <mbonnets@ensc.fr>
2016-01-20 17:10:59 +0100

    Ajout comparaison au dico

--- projet informatique/projet informatique/Program.cs
+++ projet informatique/projet informatique/Program.cs
@@ -53,7 +53,7 @@ namespace projet_informatique
 				Console.WriteLine("| PQRS  |  TUV  |   WXYZ |");
 				Console.WriteLine("_ _ _ _ _ _ _ _ _ _ _ _ _ ");
 				Console.WriteLine("|           0            |");
-				Console.WriteLine("|           _           `|");
+				Console.WriteLine("|           _            |");
 				Console.WriteLine("_ _ _ _ _ _ _ _ _ _ _ _ _ ");
 
 				Console.WriteLine("Tapez sur le pavé numérique jusqu'à la mort ou 'q' pour quitter");
@@ -92,7 +92,7 @@ namespace projet_informatique
 				previous = v;
 			}
 		}
-		public static void T9()
+		public static void T9(string mot)
 		{
 			string[] lettre = new string[] { " ", "PQRS1", "TUVùü2", "WXYZ3", "GHI4", "JKL5", "MNOñö6", ".,-?!@:()/7", "ABCàäç8", "DEFèé9" };
 			int count = 0;
@@ -113,7 +113,7 @@ namespace projet_informatique
 				Console.WriteLine("| PQRS  |  TUV  |   WXYZ |");
 				Console.WriteLine("_ _ _ _ _ _ _ _ _ _ _ _ _ ");
 				Console.WriteLine("|           0            |");
-				Console.WriteLine("|           _           `|");
+				Console.WriteLine("|           _            |");
 				Console.WriteLine("_ _ _ _ _ _ _ _ _ _ _ _ _ ");
 
 				Console.WriteLine("Tapez sur le pavé numérique jusqu'à la mort ou 'q' pour quitter");
@@ -125,93 +125,72 @@ namespace projet_informatique
 				//If the user press q, close the console
 				if (cki.KeyChar == 'q') break;
 
-
-				//Uses to permit to letter on a same number
-				/*if (cki.KeyChar == 's')
-				{
-					count = 0;
-					previous = -1;
-					continue;
-				}*/
-
 				//Todo ensure the entry is a number
-
-				//If this point is reached, the text will change
+				string dico = ouvrirFichier("../../dicoFR.txt");
 				int v = ToInt(cki.KeyChar);
 				foreach (char i in lettre[v])
 				{
-					if (i == mot [v])
+					foreach (string j in dico)
 					{
-						Console.WriteLine (mot);
+						if(i == j[count])
+						{
+							// add j to string NvDico
+						}
 					}
 				}
-
-				/*if (previous == v) //Change the last letter
-				{
-					count++;
-					result = ReplaceAt(result, result.Length - 1, lettre[v][count]);
-				}
-				else //New letter is add
-				{
-					count = 0;
-					result += lettre[v][count];
-				}*/
-
+				count++;
+				//Dico.Add(count);
 				//Need to know what is the last input
 				previous = v;
 			}
 		}
 
-		public static void ouvrirFichier(string fichier)
-		{
-			try
-			{
-				Console.WriteLine("\nOuverture du fichier : " + fichier);
+		public static string ouvrirFichier(string fichier) 
+		{ 
+			string dico = "";
+			try 
+			{ 
+				Console.WriteLine("\nOuverture du fichier : "+fichier) ;
 
 				// Création d'une instance de StreamReader pour permettre la lecture de notre fichier 
-				System.Text.Encoding encoding = System.Text.Encoding.GetEncoding("iso-8859-1");
-				StreamReader monStreamReader = new StreamReader(fichier, encoding);
+				System.Text.Encoding   encoding = System.Text.Encoding.GetEncoding(  "iso-8859-1"  );
+				StreamReader monStreamReader = new StreamReader(fichier,encoding); 
+				int nbMots = 0 ;
 
-				int nbMots = 0;
-				//int tailleMin = 100;
-				//int tailleMax = 0;
+				Console.Write("Lecture du dictionnaire, veuillez patienter...") ;
 
-				Console.Write("Lecture du dictionnaire, veuillez patienter...");
-
-				string mot = monStreamReader.ReadLine();
+				string mot = monStreamReader.ReadLine(); 
 
 				// Lecture de tous les mots du dictionnaire (un par lignes) 
-				while (mot != null)
-				{
-					nbMots++;
-
-					//if (mot.Length < tailleMin) tailleMin = mot.Length;
-					//if (mot.Length > tailleMax) tailleMax = mot.Length;
-
+				while (mot != null) 
+				{	
+					Console.WriteLine(mot);
+					nbMots++ ;
+					dico = dico + mot;
 					mot = monStreamReader.ReadLine();
 
-				}
+				} 
 				// Fermeture du StreamReader (attention très important) 
-				monStreamReader.Close();
-
-				//Console.WriteLine("\n{0} mots lus dans le fichier {1}", nbMots, fichier);
-				// Console.WriteLine("\nLe mot le plus court fait {0} lettre(s).", tailleMin);
-				//Console.WriteLine("\nLe mot le plus  long fait {0} lettre(s).", tailleMax);
-
-			}
-			catch (Exception ex)
-			{
+				monStreamReader.Close(); 
+				Console.WriteLine("Il y a {0} mots dans le dictionnaire",nbMots);
+				//Console.WriteLine(dico);
+			} 
+			catch (Exception ex) 
+			{ 
 				// Code exécuté en cas d'exception 
-				Console.Write("Une erreur est survenue au cours de la lecture :");
-				Console.WriteLine(ex.Message);
-			}
+				Console.Write("Une erreur est survenue au cours de la lecture :"); 
+				Console.WriteLine(ex.Message); 
+			} return dico;
 		}
+
+
 
 		public static void Main(string[] args)
 		{
 
-			inputLoop();
+			//inputLoop();
 			ouvrirFichier("../../dicoFR.txt");
+			//T9 ();
 
 			//string message = "";
 
